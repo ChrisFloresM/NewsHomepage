4feb3ff0d7a45213f2978f2c3bad7619bb4f616b
Christian <christian.mig.flmo@gmail.com>
2025-10-24 11:57:27 -0600

    Completed article item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import NewsComponent from './ui/NewsComponent/NewsComponent.tsx'
+import ArticleItem from './ui/ArticleItem.tsx'
+
+const testItem = {
+  imageUrl: '/img/image-retro-pcs.jpg',
+  number: 1,
+  title: 'Reviving Retro PCs',
+  summary: 'What happens when old PCs are' + ' given modern upgrades?',
+}
 
 function App() {
-  return <NewsComponent />
+  return <ArticleItem item={testItem} />
 }
 
 export default App
---
+++ src/ui/ArticleItem.tsx
@@ -0,0 +1,47 @@
+interface IArticleItem {
+  imageUrl: string
+  number: number
+  title: string
+  summary: string
+}
+
+interface IArticleItemProps {
+  item: IArticleItem
+}
+
+function ArticleItem({ item }: IArticleItemProps) {
+  const { imageUrl, number, title, summary } = item
+  return (
+    <article className="flex max-w-[340px] gap-300 bg-white">
+      <div>
+        <img
+          src={imageUrl}
+          alt={`An illustrative picture for the article ${title}`}
+        />
+      </div>
+      <section className="space-y-100">
+        <div className="space-y-100">
+          <span
+            className="text-preset-3 leading-preset-3 font-bold text-red-500"
+            aria-label={`Article number ${number}`}
+          >
+            {number.toString().padStart(2, '0')}
+          </span>
+          <h2>
+            <a
+              href="#"
+              className="text-navy-950 visited:text-navy-950 text-preset-5 leading-preset-5 font-extrabold hover:text-red-500 active:text-red-500"
+            >
+              {title}
+            </a>
+          </h2>
+        </div>
+        <p className="text-preset-6 text-navy-600 leading-preset-6">
+          {summary}
+        </p>
+      </section>
+    </article>
+  )
+}
+
+export default ArticleItem
