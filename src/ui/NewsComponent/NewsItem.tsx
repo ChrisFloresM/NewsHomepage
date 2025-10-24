15845599ebac4b5f286c3f5386a97a4f5bff0a87
Christian <christian.mig.flmo@gmail.com>
2025-10-24 11:29:37 -0600

    Fixing hover states on news component titles

--- src/ui/NewsComponent/NewsItem.tsx
+++ src/ui/NewsComponent/NewsItem.tsx
@@ -11,8 +11,13 @@ function NewsItem({ item }: INewsItemProps) {
   const { title, summary } = item
   return (
     <li className="not-first:not-last:py-400 first:pb-400 last:pt-400">
-      <h3 className="text-preset-4 leading-preset-4 text-gold-400 font-extrabold">
-        {title}
+      <h3>
+        <a
+          className="text-preset-4 leading-preset-4 hover:text-gold-400 active:text-gold-400 font-extrabold text-white transition-colors duration-200 visited:text-white"
+          href="#"
+        >
+          {title}
+        </a>
       </h3>
       <p className="text-preset-6 text-grey-300 font-normal">{summary}</p>
     </li>
